df7a927a8cd90c612a46546a1bdd00a03f219e65
Hakim Ahmed <hakimahmed123321@gmail.com>
2019-03-23 08:49:27 +0300

    Modified Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -17,9 +17,14 @@ class ProductResource extends JsonResource
         return [
             'name'     => $this->name,
             'desc'     => $this->detail,
-            'price'    => $this->price,
-            'stock'    => $this->stock,
+            'price'    => $this->price == 0 ? 'No Price' : $this->price,
+            'stock'    => $this->stock > 0 ? $this->stock : 'Out of Stock',
             'discount' => $this->discount,
+            'totalPrice'    => round((1-($this->discount/100)) *  $this->price,2),
+            'rating'   => $this->reviews->count() > 0 ?  round($this->reviews->sum('star')/$this->reviews->count()) :  "Not Rating Yet",
+            'href'     => [
+                'reviews' => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
